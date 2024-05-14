9451940f077166f97b368a4dc0c76f7887e952d9
tidev-easy <tiboyan2001@easyteka.com>
2024-05-14 16:37:45 +0300

    slider

---
+++ src/features/Slider/Slider.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="relative mb-10 slider h-64 m-0 flex gap-5">
+    <div
+      @click="slide(215)"
+      class="z-10 absolute left-0 text-2xl cursor-pointer"
+      style="transform: translate(0%, -50%); top: 50%"
+    >
+      {{ "<" }}
+    </div>
+    <div
+      class="slider relative w-100 h-64 flex"
+      :style="{ marginLeft: left + 'px' }"
+    >
+      <div class="contentslider flex p-2 rounded-md bg-red-300 h-64">
+        <h1 class="absolute left-0 text-9xl">😥</h1>
+        <h1 class="content-end text-2xl">Технические неполадки</h1>
+      </div>
+      <div class="contentslider rounded-md bg-green-300 h-64"></div>
+      <div class="contentslider rounded-md bg-green-300 h-64"></div>
+      <div class="contentslider rounded-md bg-green-300 h-64"></div>
+      <div class="contentslider rounded-md bg-green-300 h-64"></div>
+      <div class="contentslider rounded-md bg-green-300 h-64"></div>
+      <div class="contentslider rounded-md bg-green-300 h-64"></div>
+      <div class="contentslider rounded-md bg-green-300 h-64"></div>
+      <div class="contentslider rounded-md bg-green-300 h-64"></div>
+    </div>
+    <div
+      @click="slide(-215)"
+      class="absolute right-0 text-2xl cursor-pointer"
+      style="transform: translate(0%, -50%); top: 50%"
+    >
+      >
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const left = ref(0);
+
+function slide(num) {
+    if(num>0 && left.value<0) left.value = left.value + num;
+    if(num<0 && left.value>-215*9+window.innerWidth) left.value = left.value + num;
+}
+</script>
+<style lang="scss">
+.slider {
+  overflow: hidden;
+  gap: 15px;
+  transition: 0.3s;
+}
+
+.contentslider {
+  min-width: 200px;
+}
+</style>
--- src/page/Home/Home.vue
+++ src/page/Home/Home.vue
@@ -1,25 +1,26 @@
 <template>
+  <div class="">
+    <Slider />
     <div class="Home">
-        <h1>Пицца</h1>
-        <div id="pizza" class="List">
-            <CardPizza
-                :category="'Пицца'"
-                :pizza="pizza"
-                v-for="pizza in $store.state.pizza"
-                :key="pizza._id"
-            >
-                <template v-slot:type><PricePizza :pizza="pizza" /></template>
-                <template v-slot:modal="modalprops"
-                    ><PizzaWindow
-                        @close="modalprops.close"
-                        :pizza="modalprops.pizza"
-                /></template>
-            </CardPizza>
-        </div>
-        <BlockProducts id="snack" :category="'Закуски'" :name="'Закуски'" />
-        <BlockProducts id="drink" :category="'Напитки'" :name="'Напитки'" />
-        <BlockProducts id="dessert" :category="'Десерт'" :name="'Десерт'" />
+      <h1>Пицца</h1>
+      <div id="pizza" class="List">
+        <CardPizza
+          :category="'Пицца'"
+          :pizza="pizza"
+          v-for="pizza in $store.state.pizza"
+          :key="pizza._id"
+        >
+          <template v-slot:type><PricePizza :pizza="pizza" /></template>
+          <template v-slot:modal="modalprops"
+            ><PizzaWindow @close="modalprops.close" :pizza="modalprops.pizza"
+          /></template>
+        </CardPizza>
+      </div>
+      <BlockProducts id="snack" :category="'Закуски'" :name="'Закуски'" />
+      <BlockProducts id="drink" :category="'Напитки'" :name="'Напитки'" />
+      <BlockProducts id="dessert" :category="'Десерт'" :name="'Десерт'" />
     </div>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
@@ -34,38 +35,40 @@ import PricePizza from "@/shared/PricePizza/PricePizza.vue";
 import PizzaWindow from "@/entities/PizzaWindow/PizzaWindow.vue";
 import OtherWindow from "@/entities/OtherWindow/OtherWindow.vue";
 import BlockProducts from "@/features/BlockProducts/BlockProducts.vue";
+import Slider from "@/features/Slider/Slider.vue";
 
-document.title="Главная"
+document.title = "Главная";
 
 export default {
-    name: "Home",
-    components: {
-        CardPizza,
-        Modal,
-        CardOther,
-        PricePizza,
-        PizzaWindow,
-        OtherWindow,
-        BlockProducts,
-    },
-    async mounted() {
-        const pizza = await axios.get(`${domen}/pizza/getall`);
-        this.$store.commit("setPizza", pizza.data.pizzas);
-    },
+  name: "Home",
+  components: {
+    CardPizza,
+    Modal,
+    CardOther,
+    PricePizza,
+    PizzaWindow,
+    OtherWindow,
+    Slider,
+    BlockProducts,
+  },
+  async mounted() {
+    const pizza = await axios.get(`${domen}/pizza/getall`);
+    this.$store.commit("setPizza", pizza.data.pizzas);
+  },
 };
 </script>
 
 <style lang="scss">
 .Home {
-    width: 1280px;
-    margin: 0 auto;
-    @media screen and (max-width: 1300px) {
-        width: 90%;
-    }
+  width: 1280px;
+  margin: 0 auto;
+  @media screen and (max-width: 1300px) {
+    width: 90%;
+  }
 }
 .List {
-    display: flex;
-    flex-wrap: wrap;
-    gap: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
 }
 </style>
